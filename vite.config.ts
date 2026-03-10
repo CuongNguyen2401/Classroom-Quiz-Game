880a4342354c5b46c2890ad7d4a81f18d7e3e395
Cuong Nguyen <cuong_nguyenphuc@datahouse.com>
2026-03-10 23:18:01 +0700

    feat(htg-4): best practice for minimal chunk

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'mediapipe': ['@mediapipe/tasks-vision'],
+          'utils': ['sweetalert2', 'xlsx', 'canvas-confetti', 'zustand']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 })
